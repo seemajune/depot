92c3c1f215b1ad91a02247ba96c95ed008e1dd60
seemajune <seemashariat@gmail.com>
2014-11-03 10:31:06 -0500

    added first button_to for add to cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,8 +11,23 @@ class Product < ActiveRecord::Base
     message: 'acceptable image formats include .gif, .jpg or .png'
     }
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
 
     def self.latest
       Product.order(:updated_at).last
     end
+
+    private
+
+    def ensure_not_referenced_by_any_line_item
+      if self.line_items.empty?
+        true
+      else
+        errors.add(:base, 'Line Items are present')
+        false
+      end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :line_items
+
   resources :carts
 
   # The priority is based upon order of creation: first created -> highest priority.
